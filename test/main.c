444ecc36ac92793199af3fa97ec362a402d4d098
a weeb programmer <65640526+a-weeb-programmer@users.noreply.github.com>
2023-02-04 17:06:18 -0700

    Update main.c

--- test/main.c
+++ test/main.c
@@ -7,17 +7,17 @@
 #endif // _WIN32
 
 int main() {
-    CuteProfile *profile;
-    cute_log(&profile, LO, "running with libcute");
-    cute_log(&profile, ERR, "testing error logging");
-    cute_log(&profile, LOG, "testing time logging");
+    CuteProfile *profile = cute_new_dynamic_profile("test", true);
+    cute_log(profile, LOG, "running with libcute");
+    cute_log(profile, ERR, "testing error logging");
+    cute_log(profile, LOG, "testing time logging");
     sleep(3);
-    cute_log(&profile, LOG, "testing time logging");
+    cute_log(profile, LOG, "testing time logging");
     CuteProfile *dyn_profile = cute_new_dynamic_profile("dynamic-profile", true);
     cute_log(dyn_profile, LOG, "Testing dynamic profile");
     cute_log(dyn_profile, INFO, "Freeing dynamic profile");
     cute_destroy_dynamic_profile(dyn_profile);
-    cute_log(&profile, LOG, "The test is now over uwu");
+    cute_log(profile, LOG, "The test is now over uwu");
     cute_destroy_dynamic_profile(profile);
     printf("size: %zu", sizeof(CuteProfile));
     return 0;
