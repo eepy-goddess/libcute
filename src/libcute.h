edc8c709119014262dd7071d510dfaabce2f4f57
a-weeb-programmer <austinortega@outlook.com>
2023-02-06 18:52:47 -0700

    stopped relying on a glibc dependent function

--- src/libcute.h
+++ src/libcute.h
@@ -1,13 +1,13 @@
 #include <stdbool.h>
 
-enum CuteLogLevel {
+typedef enum {
     NONE,
     LOG,
     INFO,
     WARN,
     ERR, 
     DEBUG
-};
+} CuteLogLevel;
 
 typedef struct {
     unsigned int color1, color2, color3, color4, color5, color6;
@@ -22,10 +22,12 @@ typedef struct {
 
 extern CuteProfile cute_new_profile(char *name, bool show_time);
 
+extern int cute_exit_if_null(void *pointer, char *message);
+
 extern CuteProfile *cute_new_dynamic_profile(char *name, bool show_time);
 
 extern void cute_destroy_dynamic_profile(CuteProfile *profile);
 
-extern char *cute_log_level_str(enum CuteLogLevel level);
+extern char *cute_log_level_str(CuteLogLevel level);
 
-extern void cute_log(CuteProfile *profile, enum CuteLogLevel level, const char *message_fmt, ...);
+extern void cute_log(CuteProfile *profile, CuteLogLevel level, const char *message_fmt, ...);
