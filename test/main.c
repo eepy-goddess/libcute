6349ea26f5590b2e2ba97d7931d24f5109f356b9
a-weeb-programmer <austinortega@outlook.com>
2023-02-06 18:54:30 -0700

    expanded test

--- test/main.c
+++ test/main.c
@@ -2,22 +2,25 @@
 #include <stdio.h>
 #ifdef _WIN32
     #include <windows.h>
+    #define SLEEP(time) Sleep(time)
 #else // _WIN32
     #include <unistd.h>
+    #define SLEEP(time) sleep(time)
 #endif // _WIN32
 
 int main() {
     CuteProfile profile = cute_new_profile("test", true);
     cute_log(&profile, LOG, "running with libcute");
+    cute_log(&profile, INFO, "Profile name: %s", profile.name);
     cute_log(&profile, ERR, "testing error logging");
     cute_log(&profile, LOG, "testing time logging");
-    sleep(3);
+    SLEEP(3);
     cute_log(&profile, LOG, "testing time logging");
     CuteProfile *dyn_profile = cute_new_dynamic_profile("dynamic-profile", true);
     cute_log(dyn_profile, LOG, "Testing dynamic profile");
     cute_log(dyn_profile, INFO, "Freeing dynamic profile");
     cute_destroy_dynamic_profile(dyn_profile);
     cute_log(&profile, LOG, "The test is now over uwu");
-    printf("size: %zu", sizeof(CuteProfile));
+    printf("size: %zu\n", sizeof(CuteProfile));
     return 0;
 }
